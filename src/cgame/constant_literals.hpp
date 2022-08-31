0054ee7af461a8fda51df49a9a23e2db9d4d4578
FuexFollets <fuex9follets@gmail.com>
2022-08-31 19:32:59 +0000

    added game literals

--- src/cgame/constant_literals.hpp
+++ src/cgame/constant_literals.hpp
@@ -11,6 +11,18 @@ constexpr struct {
 
 constexpr struct {
     int WHITE{0},
-        BLACK{1};
+        BLACK{1},
         BLANK{3};
 } PCOLORS;
+
+constexpr struct {
+    int NORMAL{0},
+        CASTLE{1},
+        PROMOTION{2},
+        EN_PASSANT{3};
+} MTYPES;
+
+constexpr struct {
+    int KINGSIDE{0},
+        QUEENSIDE{1};
+} CTYPES;
