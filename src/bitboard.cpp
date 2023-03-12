4fd6b48fbaaa47439d5e6cbd0e5f3907b94fc98b
FuexFollets <fuex9follets@gmail.com>
2023-03-12 18:32:54 -0400

    Implemented bitboard constructor from fen position

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,11 +1,75 @@
+#include <algorithm>
 #include <bit>
-#include <iostream>
 #include <bitset>
+#include <cctype>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
 
 #include "headers/bitboard.hpp"
 
 namespace esochess {
+    bitboard::bitboard(const std::string& fen) {
+        std::string fen_position;
+        std::string fen_turn;
+        std::string fen_castle_rights;
+        std::string fen_en_passant;
+        std::string fen_halfmove_clock;
+        std::string fen_full_move_clock;
+
+        std::stringstream {fen} >> fen_position >> fen_turn >> fen_castle_rights >>
+            fen_en_passant >> fen_halfmove_clock >> fen_full_move_clock;
+
+        std::string rank;
+        int row {7};
+
+        while (std::getline(std::stringstream {fen_position}, rank, '/') && row>= 0) {
+            int column {0};
+
+            for (const char letter: rank) {
+                if (std::isdigit(letter) != 0) {
+                    const int value {letter - '0'};
+
+                    column += value;
+                }
+
+                else {
+                    const piece corresponding_piece {*std::find_if(bitboard::pieces::all.begin(),
+                            bitboard::pieces::all.end(),
+                            [letter] (bitboard::piece maybe_piece) {
+                                return maybe_piece.symbol == letter;}
+                            )};
+
+                    const std::uint64_t bit_position {1ULL << (63 - column + row * 8)};
+
+                    _pieces_bit_board[corresponding_piece.index] |= bit_position;
+                }
+            }
+        }
+
+        if (fen_turn == "w") { _turn = Turn::White; }
+        else { _turn = Turn::Black; }
+
+        if (fen_castle_rights.contains('K')) { _can_castle.white_king = true; }
+        if (fen_castle_rights.contains('Q')) { _can_castle.white_queen = true; }
+        if (fen_castle_rights.contains('k')) { _can_castle.black_king = true; }
+        if (fen_castle_rights.contains('q')) { _can_castle.black_queen = true; }
+
+        if (fen_en_passant == "-") { _en_passant_square = {}; }
+
+        else {
+            const int en_passant_cordinate_x {fen_en_passant[0] - 'a'};
+            const int en_passant_cordinate_y {fen_en_passant[0] - '1'};
+
+            _en_passant_square = 1ULL << (63 - en_passant_cordinate_x + en_passant_cordinate_y * 8);
+        }
+
+        _half_move_clock = std::atoi(fen_halfmove_clock.c_str());
+        _full_move_clock = std::atoi(fen_full_move_clock.c_str());
+    }
+
     std::array<std::uint64_t, 12> bitboard::pieces_bit_board() const {
         return _pieces_bit_board;
     }
