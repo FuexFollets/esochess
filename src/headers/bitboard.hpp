5b0656ebf9a9f83064cf2d3150e566decc416c83
FuexFollets <fuex9follets@gmail.com>
2023-03-11 20:10:43 -0500

    Added getter and setter method prototypes in header

--- src/headers/bitboard.hpp
+++ src/headers/bitboard.hpp
@@ -8,30 +8,39 @@
 
 namespace esochess::game {
     struct bitboard {
-        private:
-
         enum Turn {White, Black};
         enum BitboardIndex {
             WhitePawn, WhiteKnight, WhiteBishop, WhiteRook, WhiteQueen, WhiteKing,
             BlackPawn, BlackKnight, BlackBishop, BlackRook, BlackQueen, BlackKing
         };
 
-        std::array<std::uint64_t, 12> pieces_bit_board {};
-        Turn turn {};
-
-        struct {
+        struct castle_rights {
             bool white_king {};
             bool white_queen {};
             bool black_king {};
             bool black_queen {};
-        } can_castle {};
+        };
 
-        std::optional<std::uint64_t> en_passant_square;
+        private:
 
-        std::size_t half_move_clock {};
-        std::size_t full_move_clock {};
+        std::array<std::uint64_t, 12> _pieces_bit_board {};
+        Turn _turn {};
+
+        castle_rights _can_castle {};
+
+        std::optional<std::uint64_t> _en_passant_square;
+
+        std::size_t _half_move_clock {};
+        std::size_t _full_move_clock {};
 
         public:
+
+        [[nodiscard]] std::array<std::uint64_t, 12> pieces_bit_board() const;
+        [[nodiscard]] Turn turn() const;
+        [[nodiscard]] castle_rights can_castle() const;
+        [[nodiscard]] std::optional<std::uint64_t> en_passant_square() const;
+        [[nodiscard]] std::size_t half_move_clock() const;
+        [[nodiscard]] std::size_t full_move_clock() const;
     };
 }
 
