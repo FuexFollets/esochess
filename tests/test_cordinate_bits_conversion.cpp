f889cfaa13e8788b3815e3b03a7b5edb12c2fb92
FuexFollets <fuex9follets@gmail.com>
2023-03-13 15:11:10 -0400

    Added cordinate to bits conversion test

--- tests/test_cordinate_bits_conversion.cpp
+++ tests/test_cordinate_bits_conversion.cpp
@@ -1,3 +1,4 @@
+#include <bitset>
 #include <iostream>
 #include <vector>
 
@@ -6,9 +7,15 @@
 int main() {
     const std::vector<esochess::bitboard::cordinate> cordinates = esochess::bitboard::get_cordinates_from_bits(0b00000001'00000001'00000001'00000001'00000001'00000001'00000001'00000001);
 
+    std::cout << "Cordinates:\n";
+
     for (const esochess::bitboard::cordinate cordinate : cordinates) {
         const auto [cord_x, cord_y] {cordinate};
 
         std::cout << '(' << cord_x << ", " << cord_y << ")\n";
     }
+
+    esochess::bitboard::cordinate cordinate_0_3 {3, 0};
+
+    std::cout << std::bitset<64>(esochess::bitboard::get_bits_from_cordinate(cordinate_0_3)) << '\n';
 }
