2a585cc0290cff4d93aef51047bc17295848492d
FuexFollets <fuex9follets@gmail.com>
2023-03-16 14:33:41 -0400

    Refactored movements generator

--- src/headers/movements.hpp
+++ src/headers/movements.hpp
@@ -12,16 +12,12 @@ namespace esochess {
     bool in_bounds(int x_cordinate, int y_cordinate);
     bool in_bounds(const bitboard::cordinate& maybe_in_bounds);
 
-
-    std::vector<bitboard::cordinate> movements_from_square(std::uint64_t starting_square,
-            bitboard::piece chess_piece);
-
     std::vector<bitboard::cordinate> movements_from_square_as_pawn(bitboard::Turn color,
             std::uint64_t starting_square);
     std::vector<bitboard::cordinate> movements_from_square_as_knight(std::uint64_t starting_square);
-    std::vector<bitboard::cordinate> movements_from_square_as_bishop(std::uint64_t starting_square);
-    std::vector<bitboard::cordinate> movements_from_square_as_rook(std::uint64_t starting_square);
-    std::vector<bitboard::cordinate> movements_from_square_as_queen(std::uint64_t starting_square);
+    std::vector<std::vector<bitboard::cordinate>> movements_from_square_as_bishop(std::uint64_t starting_square);
+    std::vector<std::vector<bitboard::cordinate>> movements_from_square_as_rook(std::uint64_t starting_square);
+    std::vector<std::vector<bitboard::cordinate>> movements_from_square_as_queen(std::uint64_t starting_square);
     std::vector<bitboard::cordinate> movements_from_square_as_king(bitboard::Turn color,
             std::uint64_t starting_square);
 }
