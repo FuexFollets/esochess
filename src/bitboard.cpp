d16421951cbed425e4c2ae9ae183256cc60ecf57
FuexFollets <fuex9follets@gmail.com>
2023-04-03 09:31:06 -0400

    Added cordinate constructor

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -6,4 +6,7 @@ namespace esochess {
     std::string bitboard::piece::to_string() const {
         return std::string {symbol};
     }
+
+    bitboard::cordinate::cordinate(int pos_x, int pos_y) :
+        _x {pos_x}, _y {pos_y} {}
 }
