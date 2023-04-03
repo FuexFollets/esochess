d947261c13f380161869c7f88dd0f845492f56d9
FuexFollets <fuex9follets@gmail.com>
2023-04-03 09:57:55 -0400

    Implemented cordinate contruction from bit mask

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,3 +1,4 @@
+#include <bit>
 #include <string>
 
 #include "headers/bitboard.hpp"
@@ -9,4 +10,8 @@ namespace esochess {
 
     bitboard::cordinate::cordinate(int pos_x, int pos_y) :
         _x {pos_x}, _y {pos_y} {}
+
+    bitboard::cordinate::cordinate(bitboard::bit_representation bit_mask) :
+        _x {std::countr_zero(bit_mask) % 8},
+        _y {std::countr_zero(bit_mask / 8)} {}
 }
