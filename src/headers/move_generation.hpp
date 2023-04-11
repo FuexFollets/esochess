7ae8149d401e8aa7c3e50cb42ddb23aafb513ab6
FuexFollets <fuex9follets@gmail.com>
2023-04-11 16:33:17 -0400

    Removed redundant function argument

--- src/headers/move_generation.hpp
+++ src/headers/move_generation.hpp
@@ -25,7 +25,6 @@ namespace esochess {
 
     void bitor_add_controlled_squares(
             std::optional<bitboard::bit_representation>& controlled_squares_bits,
-            bitboard::Turn color,
             const bitboard::bit_representation& bit_mask);
 }
 
