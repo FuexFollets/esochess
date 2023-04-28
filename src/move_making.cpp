71e049bb9aae6c3820062f0ca427df5bceb96915
FuexFollets <fuex9follets@gmail.com>
2023-04-28 10:46:44 -0400

    Added move making for castling moves

--- src/move_making.cpp
+++ src/move_making.cpp
@@ -15,5 +15,36 @@ namespace esochess {
 
         return *this;
     }
-}
 
+    bitboard& bitboard::make_move(const move_castle& move) {
+        if (move.turn == Turn::White) {
+            _bitboards.at(pieces::white_king.bitboard_index) =
+                move.castle_type == CastleType::KingSide ? cordinate {"g1"}.to_bit_representation()
+                : cordinate {"c1"}.to_bit_representation();
+
+            if (move.castle_type == CastleType::KingSide) {
+                _bitboards.at(pieces::white_rook.bitboard_index) ^= cordinate {"h1"}.to_bit_representation();
+                _bitboards.at(pieces::white_rook.bitboard_index) = cordinate {"f1"}.to_bit_representation();
+            } else {
+                _bitboards.at(pieces::white_rook.bitboard_index) ^= cordinate {"a1"}.to_bit_representation();
+                _bitboards.at(pieces::white_rook.bitboard_index) = cordinate {"d1"}.to_bit_representation();
+            }
+        }
+
+        if (move.turn == Turn::Black) {
+            _bitboards.at(pieces::white_king.bitboard_index) =
+                move.castle_type == CastleType::KingSide ? cordinate {"g8"}.to_bit_representation()
+                : cordinate {"c8"}.to_bit_representation();
+
+            if (move.castle_type == CastleType::KingSide) {
+                _bitboards.at(pieces::white_rook.bitboard_index) ^= cordinate {"h8"}.to_bit_representation();
+                _bitboards.at(pieces::white_rook.bitboard_index) = cordinate {"f8"}.to_bit_representation();
+            } else {
+                _bitboards.at(pieces::white_rook.bitboard_index) ^= cordinate {"a8"}.to_bit_representation();
+                _bitboards.at(pieces::white_rook.bitboard_index) = cordinate {"d8"}.to_bit_representation();
+            }
+        }
+
+        return *this;
+    }
+}
